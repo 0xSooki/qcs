0b7c09657c5e4149e1bdb88844d06b5284657b64
Sooki <0xsooki@protonmail.com>
2024-11-15 15:38:22 +0200

    add hadamard gate

---
+++ src/gates/Hadamard.hpp
@@ -0,0 +1,20 @@
+#ifndef H_HPP
+#define H_HPP
+
+#include "Gate.hpp"
+#include <Eigen/Dense>
+#include <vector>
+
+class H : public Gate {
+public:
+  H(std::vector<int> qubits, std::vector<int> controls)
+      : Gate(2, qubits, controls) {
+    (*this)(0, 0) = 1;
+    (*this)(0, 1) = 1;
+    (*this)(1, 0) = 1;
+    (*this)(1, 1) = -1;
+    (*this) *= 1 / std::sqrt(2);
+  }
+};
+
+#endif
