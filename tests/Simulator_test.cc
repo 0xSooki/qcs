4f0ebb0686fd5f1681a192551bd88c4027178ad2
Sooki <0xsooki@protonmail.com>
2024-11-19 19:35:19 +0200

    Add tests for the simulator class

---
+++ tests/Simulator_test.cc
@@ -0,0 +1,80 @@
+#include "../src/controller/QuantumCircuit.hpp"
+#include "../src/controller/Simulator.hpp"
+#include "../src/gates/CNOT.hpp"
+#include "../src/gates/Hadamard.hpp"
+#include "../src/gates/PauliGates.hpp"
+#include "Eigen/Dense"
+#include <gtest/gtest.h>
+
+TEST(QuantumCircuitTest, AddPauliXGate) {
+  QuantumCircuit circ(1);
+  circ.addGate(std::make_shared<PauliX>(PauliX(std::vector<int>{0})));
+
+  Simulator sim;
+  Eigen::VectorXcd result = sim.run(circ);
+
+  Eigen::VectorXcd expected(2);
+  expected << 0, 1;
+  ASSERT_TRUE(result.isApprox(expected, 1e-6));
+}
+
+TEST(QuantumCircuitTest, AddHadamardGate) {
+  QuantumCircuit circ(1);
+  circ.addGate(std::make_shared<H>(H(std::vector<int>{0})));
+
+  Simulator sim;
+  Eigen::VectorXcd result = sim.run(circ);
+
+  Eigen::VectorXcd expected(2);
+  expected << 1 / sqrt(2), 1 / sqrt(2);
+  ASSERT_TRUE(result.isApprox(expected, 1e-6));
+}
+
+TEST(QuantumCircuitTest, AddCNOTGate) {
+  QuantumCircuit circ(2);
+  circ.addGate(std::make_shared<PauliX>(PauliX(std::vector<int>{0})));
+  circ.addGate(std::make_shared<CNOT>(
+      CNOT(std::vector<int>{0, 1}, std::vector<int>{0})));
+
+  Simulator sim;
+  Eigen::VectorXcd result = sim.run(circ);
+
+  Eigen::VectorXcd expected(4);
+  expected << 0, 0, 0, 1;
+  ASSERT_TRUE(result.isApprox(expected, 1e-6));
+}
+
+TEST(QuantumCircuitTest, CombinedGates) {
+  QuantumCircuit circ(3);
+  circ.addGate(std::make_shared<PauliX>(PauliX(std::vector<int>{0})));
+  circ.addGate(std::make_shared<H>(H(std::vector<int>{0})));
+  circ.addGate(std::make_shared<CNOT>(
+      CNOT(std::vector<int>{0, 2}, std::vector<int>{0})));
+
+  Simulator sim;
+  Eigen::VectorXcd result = sim.run(circ);
+
+  Eigen::VectorXcd expected(8);
+  expected << 1 / sqrt(2), 0, 0, 0, 0, -1 / sqrt(2), 0, 0;
+  ASSERT_TRUE(result.isApprox(expected, 1e-6));
+}
+
+TEST(SimulatorTest, EmptyCircuit) {
+  QuantumCircuit circ(1);
+  Simulator sim;
+  Eigen::VectorXcd result = sim.run(circ);
+
+  Eigen::VectorXcd expected(2);
+  expected << 1, 0;
+  ASSERT_TRUE(result.isApprox(expected, 1e-6));
+}
+
+TEST(SimulatorTest, ThreeQubitIdentity) {
+  QuantumCircuit circ(3);
+  Simulator sim;
+  Eigen::VectorXcd result = sim.run(circ);
+
+  Eigen::VectorXcd expected(8);
+  expected << 1, 0, 0, 0, 0, 0, 0, 0;
+  ASSERT_TRUE(result.isApprox(expected, 1e-6));
+}
